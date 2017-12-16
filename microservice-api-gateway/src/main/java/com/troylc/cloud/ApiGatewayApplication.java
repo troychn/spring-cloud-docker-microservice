d7ea3eb9f8ba2147bcb04be0102da85e2361f53f
troylc <troylc@qq.com>
2017-12-16 22:12:32 +0800

    增加springcloud部署在增加admin监控配置

--- microservice-api-gateway/src/main/java/com/troylc/cloud/ApiGatewayApplication.java
+++ microservice-api-gateway/src/main/java/com/troylc/cloud/ApiGatewayApplication.java
@@ -1,11 +1,10 @@
 package com.troylc.cloud;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
-@SpringBootApplication
+//@SpringBootApplication
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
