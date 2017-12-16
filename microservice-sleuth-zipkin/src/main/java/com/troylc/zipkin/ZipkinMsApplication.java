6cb0c7985c19ed1fe7502a04bc0cb440fc8234bb
troylc <troylc@qq.com>
2017-12-16 23:16:29 +0800

    add spring cloud sleuth -zipkin 链接追踪

---
+++ microservice-sleuth-zipkin/src/main/java/com/troylc/zipkin/ZipkinMsApplication.java
@@ -0,0 +1,12 @@
+package com.troylc.zipkin;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
+
+/**
+ * Created by troylc on 2017/12/16.
+ */
+@EnableZipkinServer
+@SpringBootApplication
+public class ZipkinMsApplication {
+}
